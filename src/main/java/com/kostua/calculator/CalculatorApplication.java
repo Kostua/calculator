3e037594023de7202f56bc607aebb1edc80ae3db
kostua <kostua.p@gmail.com>
2019-08-24 19:16:05 +0300

    Remove unused import

--- src/main/java/com/kostua/calculator/CalculatorApplication.java
+++ src/main/java/com/kostua/calculator/CalculatorApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
 
 /**
  * Entry point for run Spring Boot application
