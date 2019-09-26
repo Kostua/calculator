a7c8c674d44ced02e969c708e3f744809ae0c250
kostua <kostua.p@gmail.com>
2019-09-26 16:39:47 +0300

    Paging and sorting repostitory

--- src/main/java/com/kostua/calculator/data/CarrierRepository.java
+++ src/main/java/com/kostua/calculator/data/CarrierRepository.java
@@ -1,12 +1,12 @@
 package com.kostua.calculator.data;
 
 import com.kostua.calculator.domain.Carrier;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  *  This interface extends Spring Data JPA’s CrudRepository
  *  specifying the domain type as {@link Carrier}and the id type as Long.
  */
-public interface CarrierRepository extends CrudRepository<Carrier, Long> {
+public interface CarrierRepository extends PagingAndSortingRepository<Carrier, Long> {
 
 }
